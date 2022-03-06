f87c70e67cc584af0561c276415295f957bc3678
Shahjalal Hazari <shajalalhazari1@gmail.com>
2022-03-06 13:05:24 +0400

    Day 01 added

---
+++ Day 01/day-1-printing-start/main.py
@@ -0,0 +1,7 @@
+# Write your code below this line 👇
+print("hello! Shahjalal Hazari")
+print("How is your day going?")
+
+print("Hello World\nHello World\nHello World")
+
+print("Hello" + " " + "Shahjalal")
