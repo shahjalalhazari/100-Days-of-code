2aebfb12a17a227ea95f1048349b3bea273c0c77
Shahjalal Hazari <shajalalhazari1@gmail.com>
2022-04-04 14:37:09 +0400

    Day 25 added.

---
+++ Day 25/main.py
@@ -0,0 +1,86 @@
+import csv
+##################
+with open("weather_data.csv") as weather_data:
+    datas = weather_data.readlines()
+    for data in datas:
+        data.split()
+        print(data)
+##################
+
+
+#####################
+# In previous day we're done this task in above method.
+# But right now this method will not go to work.
+# For that we need to import Python's library called CSV.
+# Now we will complete this task in new way.
+with open("weather_data.csv") as weather_data:
+    datas = csv.reader(weather_data)
+    temps = []
+    for data in datas:
+        if data[1] != "temp":
+            temps.append(int(data[1]))
+    print(temps)
+#######################
+
+
+# We're done our task successfully.
+# But we can do it more easy way.
+# For that we have to import another library called PANDAS.
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
+print(data["temp"].mean())  # Get average of all temps
+print(data.temp.max())  # Get maximum temp
+print(data["temp"].min())  # Get minimum temp
+
+
+#  Get Data from columns
+print(data["condition"])  # 'condition' is column heading, and it should be same as column heading.
+print(data.day)
+
+#  Get Data form row
+print(data[data.day == "Monday"])
+print(data[data["day"] == "Friday"])
+
+#  Get a row of data that has maximum temp
+print(data[data.temp == data.temp.max()])
+
+#  Get condition or any value of a particular row.
+day = data[data.day == "Thursday"]
+print(day.condition)
+
+#  Get temp in Fahrenheit
+day_temp = int(day.temp)
+temp_in_f = day_temp * 9/5 + 32
+print(temp_in_f)
+
+#  Create a DataFrame from scratch
+data_dict = {
+    "Students": ["Emon", "Rubayet", "Shahjalal", "Rubayed"],
+    "scores": [89, 95, 80, 75]
+}
+data = pandas.DataFrame(data_dict)
+#  Now we to create a CSV file with datas
+data.to_csv("new_data.csv")
+print(data)
+
+
+data = pandas.read_csv("Central_Park_Squirrel_Data.csv")
+gray_color_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_color_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_color_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_color_count)
+print(red_color_count)
+print(black_color_count)
+
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_color_count, red_color_count, black_color_count]
+}
+
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("Fur Color Count.csv")
---
+++ Day 25/us-states-game/main.py
@@ -0,0 +1,41 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# Read CSV file 50 States
+data = pandas.read_csv("50_states.csv")
+# Make a list of all the states
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    state_name = screen.textinput(
+        title=f"{len(guessed_states)}/50 States Correct.",
+        prompt="What's another state name?"
+    ).title()
+
+    if state_name == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("Missing_states_list.csv")
+        break
+
+    if state_name in all_states:
+        guessed_states.append(state_name)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        # Get State data based on user input.
+        state_data = data[data.state == state_name]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_name.title())
+
+screen.exitonclick()
